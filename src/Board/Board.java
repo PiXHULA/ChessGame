47c06c8820add92f499b57e0a4417580f7f4bd7f
Joakim Briceno <joakim.onnhage@gmail.com>
2019-10-22 18:02:31 +0200

    //Working with lists in order to keep track of squares with/without pieces.

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -5,8 +5,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Board extends JPanel {
-    JPanel jpl = new JPanel();
+
     public Board(){
+        setName("CHESSGAME");
         setBackground(Color.white);
         setPreferredSize(new Dimension(BoardSize.BOARDSIZE.getSizeX()
                 , BoardSize.BOARDSIZE.getSizeY()));
@@ -15,34 +16,26 @@ public class Board extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        //TileClass board = new TileClass();
-        //board.paintComponent(g);
+        TileClass board = new TileClass();
+        board.paintComponent(g);
 
         //WHITE CHESSPLAYER
+        System.out.println(Squares.emptySquares);
         Pawn wP1 = new Pawn(Squares.B1,ChessPieceColor.WHITE);
-        /*
-        System.out.println(Squares.emptySquares);
-        System.out.println(Squares.occupiedSquares);
-        Squares.FromEmptyToOccupied(wP1.getSquare());
-        System.out.println(Squares.emptySquares);
-        System.out.println(Squares.occupiedSquares);
-        Squares.FromOccupiedToEmpty(wP1.getSquare());
         System.out.println(Squares.emptySquares);
         System.out.println(Squares.occupiedSquares);
 
-         */
-        System.out.println("från ledig: " + Squares.emptySquares);
-        System.out.println("-------------------");
-        System.out.println("från upptagen: " + Squares.occupiedSquares);
+
+
         wP1.paintPiece(g,wP1.getSquare(),wP1.getColor());
+
         wP1.moveToSquare(g,wP1.getColor(),wP1.getSquare(),Squares.A2);
-        System.out.println("från ledig: " + Squares.emptySquares);
-        System.out.println("-------------------");
-        System.out.println("från upptagen: " + Squares.occupiedSquares);
         wP1.moveToSquare(g,wP1.getColor(),wP1.getSquare(),Squares.A5);
-        System.out.println("från ledig: " + Squares.emptySquares);
-        System.out.println("-------------------");
-        System.out.println("från upptagen: " + Squares.occupiedSquares);
+        System.out.println(Squares.emptySquares);
+        System.out.println(Squares.occupiedSquares);
+
+
+
     }
 }
 
--- src/Board/BoardCreation.java
+++ src/Board/BoardCreation.java
@@ -8,6 +8,7 @@ public class BoardCreation extends JFrame {
         setTitle("Chess");
         pack();
         setVisible(true);
+        setLocation(SquareSize.SQUARESIZE.getSize() * 4, SquareSize.SQUARESIZE.getSize() / 2 );
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
